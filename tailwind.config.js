7305324a2bb9ffb79c950239a59ef7fdc183cf4b
qrulleau <quentin.rulleau@yahoo.com>
2021-03-22 11:56:12 +0100

    MIN: remove purge on tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-    './node_modules/litepie-datepicker/**/*.js',
-  ],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
