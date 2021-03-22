14ff87094bf90d0a404c1656d9b40f80a6c10cc5
qrulleau <quentin.rulleau@yahoo.com>
2021-03-22 14:16:04 +0100

    ADD: Management of 404 request

--- src/route/index.js
+++ src/route/index.js
@@ -1,12 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 import AppLogin from '../views/AppLogin.vue';
+import AppError from '../views/AppError.vue';
 
 const routes = [
   {
     path: '/',
     name: 'App',
     component: AppLogin,
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: AppError,
   },
 ];
 
